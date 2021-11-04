2691f4293beb1b6e3102c172f27d63106a30998d
Lukas Anužis <b0omasino@gmail.com>
2021-11-04 13:43:32 +0200

    Added game logic
    Added game logic

--- Testavimas2/Testavimas2/Form1.Designer.cs
+++ Testavimas2/Testavimas2/Form1.Designer.cs
@@ -43,21 +43,22 @@ namespace Testavimas2
             // 
             this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(776, 372);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(500, 500);
             this.flowLayoutPanel1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(713, 393);
+            this.button1.Location = new System.Drawing.Point(428, 556);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "Start";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(618, 393);
+            this.button2.Location = new System.Drawing.Point(333, 556);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 2;
@@ -68,7 +69,7 @@ namespace Testavimas2
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 398);
+            this.label1.Location = new System.Drawing.Point(14, 532);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(59, 13);
             this.label1.TabIndex = 3;
@@ -77,7 +78,7 @@ namespace Testavimas2
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 428);
+            this.label2.Location = new System.Drawing.Point(14, 562);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(94, 13);
             this.label2.TabIndex = 4;
@@ -85,14 +86,14 @@ namespace Testavimas2
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(77, 395);
+            this.textBox1.Location = new System.Drawing.Point(79, 529);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 5;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(112, 425);
+            this.textBox2.Location = new System.Drawing.Point(114, 559);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 6;
@@ -100,7 +101,7 @@ namespace Testavimas2
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(583, 398);
+            this.label3.Location = new System.Drawing.Point(298, 561);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(32, 13);
             this.label3.TabIndex = 7;
@@ -110,7 +111,7 @@ namespace Testavimas2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(528, 601);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
--- Testavimas2/Testavimas2/Form1.cs
+++ Testavimas2/Testavimas2/Form1.cs
@@ -12,9 +12,23 @@ namespace Testavimas2
 {
     public partial class Form1 : Form
     {
+        private Game game;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void UpdateState(string state)
+        {
+            button2.Text = game.GetTurn().ToString();
+            if (state == "Continue")
+            {
+                return;
+            }
+
+            flowLayoutPanel1.Enabled = false;
+            button2.Enabled = true;
+            MessageBox.Show(state);
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -28,5 +42,75 @@ namespace Testavimas2
                 button2.Text = "X";
             }
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                bool xTurn = true;
+                if (button2.Text == "O")
+                {
+                    xTurn = false;
+                }
+
+                game = new Game(textBox1.Text, UpdateState, textBox2.Text, xTurn);
+                PrepareTheBoard();
+                button2.Enabled = false;
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void PrepareTheBoard()
+        {
+            button2.Text = game.GetTurn().ToString();
+
+            flowLayoutPanel1.Controls.Clear();
+            flowLayoutPanel1.Enabled = true;
+            int dimension = Convert.ToInt32(textBox1.Text);
+            int buttonSize = flowLayoutPanel1.Width / dimension - 15;
+            int fontSize = (int)(buttonSize * 0.25);
+            //Console.WriteLine(fontSize);
+            for (int i = 0; i < dimension; i++)
+            {
+                for (int j = 0; j < dimension; j++)
+                {
+                    Button button = new Button();
+                    button.Tag = new Container(i, j);
+                    button.Height = buttonSize;
+                    button.Width = buttonSize;
+                    button.Font = new Font(button.Font.FontFamily, fontSize);
+                    button.MouseClick += Button_MouseClick;
+                    flowLayoutPanel1.Controls.Add(button);
+                }
+            }
+        }
+
+        private void Button_MouseClick(object sender, MouseEventArgs e)
+        {
+            Button b = (Button)sender;
+            Container c = (Container)b.Tag;
+
+            game.SetGridValue(c.X, c.Y);
+            b.Text += game.GetGridValue(c.X, c.Y);
+            b.Enabled = false;
+        }
+    }
+
+    class Container
+    {
+        private int x;
+        private int y;
+
+        public Container(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int X { get => x; set => x = value; }
+        public int Y { get => y; set => y = value; }
     }
 }
